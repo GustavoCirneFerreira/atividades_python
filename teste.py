7f961ecc7812a738558f584a89a536ac1240f95e
Gustavo Cirne Ferreira <114815870+GustavoCirneFerreira@users.noreply.github.com>
2023-05-22 19:17:38 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,8 +0,0 @@
-score = int(input("Diga seu pontos:"))
-
-if score >= 75:
-    print("You have passed you exam")
-    print("Congrats")
-
-if score < 75:
-    print("Better luck next time, you failed.")
