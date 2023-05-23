d7e209d53afd74ca96f511bab920b513e956d9fe
Gustavo Cirne Ferreira <114815870+GustavoCirneFerreira@users.noreply.github.com>
2023-05-22 21:22:35 -0300

    Create atividade_input.py

---
+++ atividade_input.py
@@ -0,0 +1,11 @@
+print("")
+nome = input("Bom dia! Qual é o seu nome? ")
+print("Olá ", nome, ", irei responder em qual ano você estará com 100 anos de idade.\n")
+
+ano_nasci = int(input("Em que ano você nasceu? "))
+
+idade = ano_nasci + 100
+
+print(f"{nome} terá 100 anos de idade em: {idade}")
+
+
