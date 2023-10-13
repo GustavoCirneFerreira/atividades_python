c8246baf4f5bb02ac65cf707ccd430be6c92771b
Gustavo Cirne Ferreira <114815870+GustavoCirneFerreira@users.noreply.github.com>
2023-10-13 16:54:20 -0300

    Update atividade_função.py

--- atividade_função.py
+++ atividade_função.py
@@ -16,6 +16,7 @@ s = somav1v2(v1, v2)
 print("A soma dos dois números é de:", s)
 """
 
+"""
 def ParOuImpar(v):
     if (v % 2 == 0):
         return ("Par")
@@ -25,6 +26,17 @@ def ParOuImpar(v):
 n = int(input("Digite um número: "))
 r = ParOuImpar(n)
 print("O número", n, "é um número", r)
+"""
+
+def fatorial(v):
+    r = 1
+    for c in range(1, v):
+        r = r * c
+    return r
+
+n = int(input("Digite um número: "))
+f = fatorial(n)
+print("O valor de", n, "é igual a:", f)
 
 
 
